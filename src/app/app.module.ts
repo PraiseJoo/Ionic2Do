8cb8a622e12f299eb0c80106ca823993027eacec
PraiseJoo <21300744@handong.edu>
2017-10-10 02:30:27 +0900

    Finally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,43 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { TaskListPage } from '../pages/tasklist/tasklist';
+
+import { Dialogs } from '@ionic-native/dialogs';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaQCLh2nmZQoD_es2ZOj07U5VBi4Rhol0",
+  authDomain: "ionic2do-7efac.firebaseapp.com",
+  databaseURL: "https://ionic2do-7efac.firebaseio.com",
+  projectId: "ionic2do-7efac",
+  storageBucket: "ionic2do-7efac.appspot.com",
+  messagingSenderId: "717072248640"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TaskListPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TaskListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Dialogs,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
